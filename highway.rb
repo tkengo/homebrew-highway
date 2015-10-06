73582d39fc536e8567708a6c78cf62d57aa9850c
Kengo Tateishi <embrace.ddd.flake.peace@gmail.com>
2015-10-06 14:08:41 +0900

    First commit

---
+++ highway.rb
@@ -0,0 +1,38 @@
+# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
+#                http://www.rubydoc.info/github/Homebrew/homebrew/master/frames
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Highway < Formula
+  desc "High performance source code search tool."
+  homepage "https://github.com/tkengo/highway/"
+  url "https://github.com/tkengo/highway/archive/master.zip"
+  sha256 ""
+
+  # depends_on "cmake" => :build
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "gperftools" => :build
+
+  def install
+    system "aclocal", "-I #{HOMEBREW_PREFIX}/share/aclocal"
+    system "autoconf"
+    system "autoheader"
+    system "automake", "--add-missing"
+    system "./configure", "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test highway`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
