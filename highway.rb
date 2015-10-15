4e1d2dbb3519af1635daf2242f44e7d87deb8204
Kengo Tateishi <embrace.ddd.flake.peace@gmail.com>
2015-10-15 16:19:11 +0900

    Updated sha256 hash

--- highway.rb
+++ highway.rb
@@ -6,7 +6,7 @@ class Highway < Formula
   desc "High performance source code search tool."
   homepage "https://github.com/tkengo/highway/"
   url "https://github.com/tkengo/highway/archive/v1.0.0.tar.gz"
-  sha256 "5fd0c79e18e8657100fb3fbca554747b4b95b37087d3bba58fa8082842767c08"
+  sha256 "2925d97adabd16c20fb1142ab8608841fc09397fb8ffc77e17bce97cf2554e87"
 
   version "1.0.0"
 
