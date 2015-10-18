6b4b06b73ee6beac611d07aae6c4ecf497cf3888
Kengo Tateishi <embrace.ddd.flake.peace@gmail.com>
2015-10-18 13:59:54 +0900

    Updates sha256 hash and added test block

--- highway.rb
+++ highway.rb
@@ -1,12 +1,8 @@
-# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
-#                http://www.rubydoc.info/github/Homebrew/homebrew/master/frames
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Highway < Formula
   desc "High performance source code search tool."
   homepage "https://github.com/tkengo/highway/"
   url "https://github.com/tkengo/highway/archive/v1.0.0.tar.gz"
-  sha256 "5c77b2e8037e65eb0658121c00c97f527182c308c6e0d55e3e8596d89cafd564"
+  sha256 "611d00ffca0741d219c09e841a855e5d78b896493a51bc8691dbc850237dabbe"
 
   version "1.0.0"
 
@@ -25,15 +21,10 @@ class Highway < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test highway`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    path = testpath/"hello_world.txt"
+    path.write "Hello World!"
+
+    lines = `#{bin}/hw 'Hello World!' #{path}`.strip.split(":")
+    assert_equal "Hello World!", lines[2]
   end
 end
